828fae343970217c5795dee92dc8777385c67165
Student528 <5281866@student.gloscol.ac.uk>
2026-03-25 12:13:31 +0000

    Slidy Jump

--- Assets/Player/Player Move.cs
+++ Assets/Player/Player Move.cs
@@ -2,6 +2,7 @@ using System;
 using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
 
 public class PlayerMove : MonoBehaviour
 {
@@ -13,9 +14,11 @@ public class PlayerMove : MonoBehaviour
     public float groundDrag;
 
 
+
     public float jumpForce;
     public float jumpCooldown;
     public float airMultiplier;
+    bool DisablePlaneMovement;
     bool readyToJump;
 
 
@@ -25,11 +28,14 @@ public class PlayerMove : MonoBehaviour
  
 
     [Header("GroundCheck")]
+
     public float playerHeight;
+    public Collider Collider;
     public LayerMask WhatIsGround;
-    bool grouded;
-
-
+    bool grounded;
+    private bool PreviousGrounded;
+    private bool ApplyFriction = false;
+    private float Friction_Until;
     public Transform orientation;
 
     float horizontalInput;
@@ -45,28 +51,62 @@ public class PlayerMove : MonoBehaviour
         rb = GetComponent<Rigidbody>();
         rb.freezeRotation = true;
         JumpAction = InputSystem.actions.FindAction("Jump");
+        Invoke(nameof(ResetJump), jumpCooldown);
+        DisablePlaneMovement = false;
     }
 
 
     private void FixedUpdate()
     {
         MovePlayer();
-    }
+
+        if (PreviousGrounded != grounded)
+        {
+
+            ApplyFriction = true;
+            Friction_Until = Time.time + 2f;
+        }
+        if (ApplyFriction == true)
+            {
+                if (Time.time <= Friction_Until)
+                {
+                    Collider.material.staticFriction = 100f;
+                }
+                else
+                {
+                    ApplyFriction = false;
+                    Collider.material.staticFriction = 0f;
+                }
+            }
+        }
+
 
     private void Update()
     {  //Ground Check :P
-        grouded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, WhatIsGround);
-
-        print(JumpAction.WasPerformedThisFrame());
-        MyInput();
+        PreviousGrounded = grounded;
+        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, WhatIsGround);
+        
+        
         SpeedControl();
+    
+            if (DisablePlaneMovement == false && grounded)
+            {
+                MyInput();
+
+            }
+        
+
+
+
         //handle drag
-        if (grouded)
+        if (grounded)
             rb.linearDamping = groundDrag;
         else
             rb.linearDamping = 0;
+ 
 
 
+  
     }
 
 
@@ -76,11 +116,13 @@ public class PlayerMove : MonoBehaviour
         horizontalInput = Input.GetAxisRaw("Horizontal");
         verticalInput = Input.GetAxisRaw("Vertical");
         
-        if (JumpAction.WasPerformedThisFrame() && readyToJump && grouded)
+        if (JumpAction.WasPerformedThisFrame() && readyToJump && grounded)
         {
-            print("JUMP NOW");
+
             readyToJump = false;
+            DisablePlaneMovement = true;
             Jump();
+           
             Invoke(nameof(ResetJump), jumpCooldown);
         }
     }
@@ -88,12 +130,18 @@ public class PlayerMove : MonoBehaviour
     {
         moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
 
-        //on ground
-        if (grouded)
-         rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
+   
 
-        else if (!grouded)
+        //on ground
+        if (grounded)
+        {
+            rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
+        }
+        else
+        {
             rb.AddForce(moveDirection.normalized * moveSpeed * 10f * airMultiplier, ForceMode.Force);
+          
+        }
     }
 
     private void SpeedControl()
@@ -111,9 +159,12 @@ public class PlayerMove : MonoBehaviour
     {
         rb.linearVelocity = new Vector3(rb.linearVelocity.x,0f,rb.linearVelocity.z);
         rb.AddForce(transform.up*jumpForce, ForceMode.Impulse);
+        
     }
     private void ResetJump()
     {
         readyToJump = true;
+        DisablePlaneMovement = false;
+        
     }
 }
